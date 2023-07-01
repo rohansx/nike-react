5f24b72164efd4be15995aeffe991c7a8d63865a
rohansx <sharmarohan615@gmail.com>
2023-07-01 18:18:36 +0530

    add store.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import './App.css';
-import { Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { responsiveFontSizes } from "@mui/material";
+import "./App.css";
+import { globalTheme } from "./style/theme";
+import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
+import WelcomeMessage from "./components/Welcome/WelcomeMessage";
+import Promotion from "./components/Promotion/Promotion";
+import ProductCard from "./components/ProductsCard/ProductsCard";
+import Categories from "./components/Categories/Categories.jsx";
+import Featured from "./components/Featured/Featured";
+import JoinUs from "./components/JoinUs/JoinUs";
+import MoreNike from "./components/MoreNike/MoreNike";
+import ShopLinks from "./components/ShopLinks/ShopLinks";
 import Footer from "./components/Footer/Footer";
-import NavigationMenu from './components/NavigationMenu/NavigationMenu';
-import WelcomeMessage from './components/Welcome/WelcomeMessage';
-import ProductCard from './components/ProductsCard/ProductsCard';
-import Featured from './components/Featured/Featured';
-import JoinUs from './components/JoinUs/JoinUs';
-import MoreNike from './components/MoreNike/MoreNike';
-// import Promotion from './components/Promotion/Promotion';
-
+//
 
 function App() {
+  let theme = createTheme(globalTheme);
+  theme = responsiveFontSizes(theme);
+  // console.log("/", theme);
   return (
-    <>
-      <NavigationMenu />
-      <WelcomeMessage />
-      {/* <Promotion /> */}
-      {/* <Featured /> */}
-      <ProductCard />
-      {/* <JoinUs /> */}
-      <MoreNike />
-      <Footer />
-    </>
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <NavigationMenu />
+        <WelcomeMessage />
+        <Promotion />
+        <ProductCard />
+        <Categories />
+        <Featured />
+        <JoinUs />
+        <MoreNike />
+        <ShopLinks />
+        <Footer />
+      </ThemeProvider>
+    </div>
   );
 }
 
