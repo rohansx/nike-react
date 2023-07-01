cdc348a95694e011bbeefdd641c9d156e6761d53
rohansx <sharmarohan615@gmail.com>
2023-07-01 11:59:16 +0530

    Intialize react app and add footer

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./components/Footer/Footer";
+import { Typography } from "@mui/material";
+
+
+function App() {
+  return (
+    <>
+      <h1>
+        HEllo
+      </h1>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
